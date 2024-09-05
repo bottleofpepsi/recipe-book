114fc1fb9dcb0ceda191500478652ee898a5cd30
sirensong0 <smalenskimikalaj@gmail.com>
2024-09-05 11:47:59 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import ErrorBoundary from "@/components/ErrorBoundary";
+import ErrorPage from "@/pages/ErrorPage";
+import RecipeInfoPage from "@/pages/RecipeInfoPage";
+import SearchPage from "@/pages/SearchPage";
 
 function App() {
-    return <></>;
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <ErrorBoundary>
+                        <SearchPage />
+                    </ErrorBoundary>
+                }
+            />
+            <Route
+                path="/:id"
+                element={
+                    <ErrorBoundary>
+                        <RecipeInfoPage />
+                    </ErrorBoundary>
+                }
+            />
+            <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+    );
 }
 
 export default App;
